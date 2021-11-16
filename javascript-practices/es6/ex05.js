9440b83b6daa8f8484afe98aa071b574d5ea6b8c
hakyeonghoon1 <skyhoon8@gmail.com>
2021-11-16 14:20:10 +0900

    async

--- javascript-practices/es6/ex05.js
+++ javascript-practices/es6/ex05.js
@@ -29,3 +29,28 @@ numbers.forEach(function(n){
 })
 console.log(pow(5));
 //ex2
+console.log("\n-----\n");
+numbers.forEach(n=>process.stdout.write(`${n}:${(x=>x*x)(n)}\t`));
+
+//ex3 - 여러 행 함수
+console.log("\n-----\n");
+[5,3,15,1212,34,92].forEach(e=>{
+    if(e % 5 ==0){
+        process.stdout.write(`${e}\t`);
+    }
+})
+
+// ex4 - this를 어휘적 바인딩(Lexical Bind)
+console.log("\n------\n");
+const dooly={
+    name:'둘리',
+    friends:['또치','도우너','마이콜','길동'],
+    printFriends:function(){
+       // this.friends.forEach(function(f){
+       //     console.log(`${this.name}의 친구 ${f}`);
+       // }.bind(this));
+
+        this.friends.forEach(f=>{console.log(`${this.name}의 친구 ${f}`)});
+    }
+}
+dooly.printFriends();
