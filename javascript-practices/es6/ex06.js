e9fb1214b0b60ff46659e7b406b16afc7f106d65
hakyeonghoon1 <skyhoon8@gmail.com>
2021-11-17 11:06:07 +0900

    구조분해

--- javascript-practices/es6/ex06.js
+++ javascript-practices/es6/ex06.js
@@ -6,6 +6,66 @@ const user = {
     lastName:'리',
     email:'dooly@gmail.com'
 };
-({firstName, lastName}=user);
+// let firstName = user.firstName;
+// let lastName = user.lastName;
+//({firstName, lastName}=user);
+
+const {firstName, lastName} = user;
 
 console.log(firstName,lastName);
+
+// ex02 - Default Value
+const goods ={
+    name:'TV',
+    price:10000,
+    stockCount:30
+}
+
+let {name,price,countStok=0,stockCount=0} = goods;
+
+console.log(name, price, countStok, stockCount);
+
+// ex03 - 구조 분해해야 할 객체의 속성과 변수 이름이 다를 때
+const person = {
+    name:'하경훈',
+    country:'korea'
+}
+
+const {name:fullName, country:place} = person;
+console.log(fullName,place);
+
+// ex04 - 내부 객체(Nested Object)의 구조 분해
+const student = {
+    name:'둘리',
+    age:10,
+    score:{
+        math:90,
+        korean:80,
+        science:70
+    }
+};
+
+const {
+    name:studentName, 
+    score:{
+        math=0,
+        korean=0,
+        science=0,
+        music=0
+    }
+} = student;
+console.log(`${studentName}의 성적
+수학:${math}
+국어:${korean}
+과학:${science}
+음악:${music}
+`);
+
+//ex05 - 함수의 파라미터
+const averageScore = function(o){
+    console.log(`${o.name}의 평균은 ${(o.score.math+o.score.korean+o.score.science)/3}입니다.`);
+};
+
+averageScore(student);
+
+// ex06 - 배열...
